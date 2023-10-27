311e3b1f39b9cccd22c2ddfcc266c2d493726fa7
liginity <liginity@outlook.com>
2023-10-27 21:14:45 +0800

    make build-image a wrapper for docker build
    now extra arguments are passed to docker build.

--- build-helper.sh
+++ build-helper.sh
@@ -4,14 +4,14 @@
 set -euo pipefail
 
 function _build-image-usage () {
-    printf "usage: build-image dockerfile [image_name]\n" >&2
+    printf "usage: build-image dockerfile image_name [more-docker-build-args]\n" >&2
 }
 
 function build-image () {
     # $1: docker file name
     # $2: image name
 
-    if [ $# -lt 1 ]; then
+    if [ $# -lt 2 ]; then
         printf "missing argument\n" >&2
         _build-image-usage
         exit 1
@@ -21,9 +21,12 @@ function build-image () {
 
     dockerfile_name="$1"
     # if image_name is omitted, then dockerfile_name should be like "image_name-Dockerfile".
-    image_name="${2:-${dockerfile_name%-Dockerfile}}"
+    # image_name="${2:-${dockerfile_name%-Dockerfile}}"
+    image_name="$2"
+    shift 2
+
     timestamp=$(date +"${timestamp_format}")
     tag_name="date-${timestamp}"
 
-    docker build -f "${dockerfile_name}" -t "${image_name}" -t "${image_name}:${tag_name}" .
+    docker build -f "${dockerfile_name}" -t "${image_name}" -t "${image_name}:${tag_name}" "$@" .
 }
